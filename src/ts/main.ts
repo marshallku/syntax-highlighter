63a2c6022083fe4a805936bb7dec81d68fc808cd
marshall-ku <marshall@kakao.com>
2021-09-10 20:22:36 +0900

    Implement loader

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,10 +5,17 @@ import "../css/style.css";
 
 async function initialize() {
     const root = document.getElementById("root");
+    const loader = document.querySelector(".loader");
     await initializeHighlighter();
 
     if (!root) return;
 
     root.append(App());
+
+    if (!loader) return;
+    loader.addEventListener("transitionend", () => {
+        loader.remove();
+    });
+    loader.classList.add("loader--hide");
 }
 initialize();
