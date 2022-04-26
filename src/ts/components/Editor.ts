d3f49305945a9658282a519e0ebf822f386e666b
marshall-ku <marshall@kakao.com>
2022-04-26 10:33:17 +0900

    Don't use anonymous functions for redundant logic

--- src/ts/components/Editor.ts
+++ src/ts/components/Editor.ts
@@ -2,75 +2,60 @@ import el from "../utils/el";
 import { data, highlight, highlightAndCopy } from "../utils/highlighter";
 
 export default function Editor() {
+    const handleChange = (event: Event) => {
+        const { target } = event;
+
+        if (!(target instanceof HTMLTextAreaElement)) {
+            return;
+        }
+
+        data.code = target.value;
+        highlight();
+    };
+
+    const handleKeyDown = (event: Event) => {
+        const { code, target } = event as KeyboardEvent;
+
+        if (code !== "Tab" || !(target instanceof HTMLTextAreaElement)) {
+            return;
+        }
+
+        event.preventDefault();
+
+        const currentStartPosition = target.selectionStart;
+        const currentEndPosition = target.selectionEnd;
+        const currentValue = target.value;
+
+        target.value = `${currentValue.slice(
+            0,
+            currentStartPosition
+        )}  ${currentValue.slice(currentStartPosition)}`;
+        target.selectionEnd = currentEndPosition + 2;
+        highlight();
+    };
+
+    const handleCopyAndPaste = (event: Event) => {
+        const { target } = event;
+
+        if (!(target instanceof HTMLTextAreaElement)) {
+            return;
+        }
+
+        data.code = target.value;
+        highlightAndCopy();
+    };
+
     return el("textarea", {
         autocapitalize: "off",
         spellcheck: false,
         autocomplete: "off",
         value: data.code,
         events: {
-            keyup: (event) => {
-                const { target } = event;
-
-                if (!(target instanceof HTMLTextAreaElement)) {
-                    return;
-                }
-
-                data.code = target.value;
-                highlight();
-            },
-            change: (event) => {
-                const { target } = event;
-
-                if (!(target instanceof HTMLTextAreaElement)) {
-                    return;
-                }
-
-                data.code = target.value;
-                highlight();
-            },
-            keydown: (event) => {
-                const { code, target } = event as KeyboardEvent;
-
-                if (
-                    code !== "Tab" ||
-                    !(target instanceof HTMLTextAreaElement)
-                ) {
-                    return;
-                }
-
-                event.preventDefault();
-
-                const currentStartPosition = target.selectionStart;
-                const currentEndPosition = target.selectionEnd;
-                const currentValue = target.value;
-
-                target.value = `${currentValue.slice(
-                    0,
-                    currentStartPosition
-                )}  ${currentValue.slice(currentStartPosition)}`;
-                target.selectionEnd = currentEndPosition + 2;
-                highlight();
-            },
-            paste: (event) => {
-                const { target } = event;
-
-                if (!(target instanceof HTMLTextAreaElement)) {
-                    return;
-                }
-
-                data.code = target.value;
-                highlightAndCopy;
-            },
-            copy: (event) => {
-                const { target } = event;
-
-                if (!(target instanceof HTMLTextAreaElement)) {
-                    return;
-                }
-
-                data.code = target.value;
-                highlightAndCopy;
-            },
+            keyup: handleChange,
+            change: handleChange,
+            keydown: handleKeyDown,
+            paste: handleCopyAndPaste,
+            copy: handleCopyAndPaste,
         },
     });
 }
