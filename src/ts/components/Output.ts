071906f241adb8e24f06a1eeed9e347f781618e1
marshallku <marshall@kakao.com>
2022-05-08 18:45:19 +0900

    Don't pass useless arguments

--- src/ts/components/Output.ts
+++ src/ts/components/Output.ts
@@ -2,7 +2,7 @@ import el from "../utils/el";
 import { highlighter } from "../utils/highlighter";
 
 export default function Output() {
-    const div = el("div", {});
+    const div = el("div");
 
     highlighter.addOutputElement(div);
 
